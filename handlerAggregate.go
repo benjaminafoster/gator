24c2acb38e0de3d1120e073f55d23e3f9f30cf2d
Ben Foster <benjaminafoster4@gmail.com>
2026-02-12 16:39:01 -0500

    completed agg command implementation

--- handlerAggregate.go
+++ handlerAggregate.go
@@ -1,26 +1,29 @@
 package main
 
 import (
-	"fmt"
 	"context"
+	"fmt"
 	"html"
+	"time"
 )
 
 func handlerAggregate(s *State, cmd Command) error {
-	if len(cmd.args) != 0 {
+	if len(cmd.args) != 1 {
 		return fmt.Errorf("Usage: gator agg\n")
 	}
 	
-	feedUrl := "https://www.wagslane.dev/index.xml"
-	
-	feed, err := fetchFeed(context.Background(), feedUrl)
+	time_between_reqs := cmd.args[0]
+	parsedDuration, err := time.ParseDuration(time_between_reqs)
 	if err != nil {
-		return fmt.Errorf("failed to fetch feed: %w\n", err)
+		return fmt.Errorf("failed to parse duration: %w\n", err)
 	}
 	
-	fmt.Println(unescapeHTML(feed))
+	fmt.Printf("Collecting feeds every %s...\n", parsedDuration)
 	
-	return nil
+	ticker := time.NewTicker(parsedDuration)
+	for ; ; <- ticker.C {
+		scrapeFeeds(s)
+	}
 }
 
 func unescapeHTML(feed *RSSFeed) *RSSFeed {
@@ -33,4 +36,33 @@ func unescapeHTML(feed *RSSFeed) *RSSFeed {
 	}
 
 	return feed
+}
+
+func scrapeFeeds(s *State) error {
+	nextFeed, err := s.db.GetNextFeedToFetch(context.Background())
+	if err != nil {
+		return fmt.Errorf("failed to determine the next feed to fetch: %w\n", err)
+	}
+	
+	nextFeedID := nextFeed.ID
+	
+	err = s.db.MarkFeedFetched(context.Background(), nextFeedID)
+	if err != nil {
+		return fmt.Errorf("failed to mark feed as fetched: %w\n", err)
+	}
+	
+	feed, err := fetchFeed(context.Background(), nextFeed.Url)
+	if err != nil {
+		return fmt.Errorf("failed to fetch feed: %w\n", err)
+	}
+	
+	unescapedFeed := unescapeHTML(feed)
+	
+	fmt.Printf("Printing posts found in %s...\n", unescapedFeed.Channel.Title)
+	
+	for _, item := range unescapedFeed.Channel.Item {
+		fmt.Printf("Found post: %s\n", item.Title)
+	}
+	
+	return nil
 }
